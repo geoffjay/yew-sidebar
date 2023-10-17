bba3f186877624e926d99d353b5b322629a9e033
geoffjay <geoffjay@users.noreply.github.com>
2023-10-17 04:05:15 +0000

    deploy: fb0cfe06952939f35e61a0781167ec980375ebcf

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use yew_sidebar::app::App;
-
-// This is the entry point for the web app
-fn main() {
-    wasm_logger::init(wasm_logger::Config::default());
-    yew::Renderer::<App>::new().render();
-}
